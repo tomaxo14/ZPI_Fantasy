af90a5b46b359f50c5f5a32066db38c8775838aa
tomaxo14 <tomek.przybylski@onet.pl>
2020-04-19 19:28:26 +0200

    first testing

--- src/main/java/com/example/ZPI/Controller/AthleteController.java
+++ src/main/java/com/example/ZPI/Controller/AthleteController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AthleteController {
@@ -15,5 +16,10 @@ public class AthleteController {
     @GetMapping("/athletes")
     public ResponseEntity<?> athletes() {
         return ResponseEntity.ok(athleteService.getAllAthletes());
+    }
+
+    @GetMapping("/athleteDetails")
+    public ResponseEntity<?> athleteDetails(@RequestParam int athleteId){
+        return ResponseEntity.ok(athleteService.getAthlete(athleteId));
     }
 }
